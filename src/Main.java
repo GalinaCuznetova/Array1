8b261d775c90ea37e8c773184ede921c5ebded80
Galina_c <galina__k@mail.ru>
2022-09-19 22:35:41 +0300

    Исправлено ДЗ

--- src/Main.java
+++ src/Main.java
@@ -10,26 +10,33 @@ public class Main {
         System.out.println(mileage[4]);
         //Задание1.2
         System.out.println("Задание 1.2");
-          for (int i = 1; i < height.length; i++) {
-                 System.out.print(height[i] -1 + ",");
-                   if (i == height.length - 1) {
+          for (int i = 0; i < height.length; i++) {
+              if (i == height.length - 1) {
                   System.out.print(height[i]);
+                 break;
               }
+              System.out.print(height[i] + ",");
+
           }
         System.out.println(" ");
-        for (int i = 1; i < consistence.length; i++) {
-            System.out.print(consistence[i] -1 + ",");
+        for (int i = 0; i < consistence.length; i++) {
             if (i == consistence.length - 1) {
                 System.out.print(consistence[i]);
+                break;
             }
+            System.out.print(consistence[i] + ",");
+
         }
         System.out.println(" ");
-        for (int i = 1; i < mileage.length; i++) {
-            System.out.print(mileage[i] -1 + ",");
+        for (int i = 0; i < mileage.length; i++) {
             if (i == mileage.length - 1) {
                 System.out.print(mileage[i]);
+                break;
             }
+            System.out.print(mileage[i] + ",");
+
         }
+
          //Задание 1.3
         System.out.println(" ");
         System.out.println("Задание 1.3");
@@ -62,9 +69,10 @@ public class Main {
         for ( int i = 0; i < height.length; i++) {
             if (height[i] % 2 != 0) {
                 height[i] = height[i] + 1;
+            }
                 System.out.println(height[i]);
             }
-        }
+
 
 
 
